18a3b6a818521d0e9bc5a5bdbc8e974b0e177c3b
moisesreis <bk_moises@hotmail.com>
2023-01-05 14:51:33 -0300

    Inclusão da linha de inicialização do db

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const connection = require('./database/database');
+const perguntaModel = require('./database/Pergunta');
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão estabelecida com sucesso!")
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render("index");
@@ -10,7 +25,14 @@ app.get('/', (req, res) => {
 
 app.get('/ask', (req, res) => {
   res.render('perguntar');
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+
+  res.send("Formulário recebido" + titulo + descricao);
 });
+
+app.post('/salvarpergunta', (req, res) => {
+})
 
 app.listen(4000, error => {
   if (error) console.log("Ops, algo deu errado!")
